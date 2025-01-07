7d5c99d952e65ceed8d337ac65aa1e752929178a
unknown <arnolcb14@gmail.com>
2025-01-07 09:17:30 -0500

    v1.1 - Use cookies

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def descargar_video_youtube(url):
     ydl_opts = {
         'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
         'outtmpl': 'video.%(ext)s',  # Se descarga como video.mp4
-        'quiet': True
+        'quiet': True,
+        'cookiesfile': 'cookies.txt'
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
